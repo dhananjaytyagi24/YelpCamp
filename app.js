9c9a6c58da155d04b7291f9168e72ba6a9d17701
dhananjaytyagi24 <dhananjaytyagi24@gmail.com>
2019-04-16 22:21:31 -0700

    second commit till v10

--- app.js
+++ app.js
@@ -1,101 +1,54 @@
-var express= require("express");
-var app= express();
-var bodyparser= require("body-parser");
-var mongoose= require("mongoose");
-var Campground= require("./models/campground");
-var seedDB= require("./seeds");
-var Comment= require("./models/comment");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, {useNewUrlParser: true});
 
-seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
-app.get("/", function(req, res){
-   res.render("landing"); 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Piku and Avi",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
 });
 
-app.get("/campgrounds", function(req, res){
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});    
-        }
-    });
-});
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.post("/campgrounds", function(req, res){
-    var name= req.body.name;
-    var image= req.body.image;
-    var desc= req.body.description;
-    var newCampground= {name: name, image: image, description: desc};
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err)
-            console.log(err);
-        else
-            res.redirect("/campgrounds");
-    });
-});
-
-app.get("/campgrounds/new", function(req, res) {
-    res.render("campgrounds/new");
-});
-
-app.get("/campgrounds/:id", function(req, res) {
-    Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground){
-       if(err){
-           console.log(err);
-       }
-       else{
-            res.render("campgrounds/show", {campground: foundCampground});    
-       }
-    });
-}); 
-
-//========================
-// Comment Routes
-//========================
-
-app.get("/campgrounds/:id/comments/new", function(req, res) {
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("comments/new",{campground: campground});        
-        }
-    });
-});
-
-app.post("/campgrounds/:id/comments", function(req, res) {
-    //lookup campground using Id
-    //create new comment
-    // connect new comment to campground
-    // redirect campground show page
-    
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err);
-            res.redirect("/campgrounds");
-        }
-        else{
-            Comment.create(req.body.comment, function(err, comment){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    campground.comments.push(comment);
-                    campground.save();
-                    res.redirect('/campgrounds/'+ campground._id);
-                }
-            })      
-        }
-    });
-});
 
 const port = process.env.PORT || '3000';
 app.listen(port, function () {
